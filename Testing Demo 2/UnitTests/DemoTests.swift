46c827d1a79104bb7a27c4dc3d717018d403496e
Ben Scheirman <ben@scheirman.com>
2018-03-09 10:17:19 -0600

    Episode 329

---
+++ Testing Demo 2/UnitTests/DemoTests.swift
@@ -0,0 +1,25 @@
+//
+//  DemoTests.swift
+//  UnitTests
+//
+//  Created by Ben Scheirman on 2/23/18.
+//  Copyright © 2018 Fickle Bits, LLC. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Testing_Demo_2
+
+class DemoTests : XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testDemo1() {
+        XCTAssertEqual(1, 1)
+    }
+}
